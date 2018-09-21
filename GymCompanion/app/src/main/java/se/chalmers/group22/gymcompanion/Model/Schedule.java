7a8c7f0f1f58ceaa98195de8db696f8aa5e30b92
assfucker69 <minemail>
2018-09-21 12:08:15 +0200

    Added class Schedule

---
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/Schedule.java
@@ -0,0 +1,13 @@
+package se.chalmers.group22.gymcompanion.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Schedule {
+
+    private List<Exercise> exercises = new ArrayList<>();
+
+    public Schedule(){
+        
+    }
+}
