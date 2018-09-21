f4e454dbb1d5454ed053b5618c53b22b237620de
ChalmersBock <erikboc@student.chalmers.se>
2018-09-21 11:43:29 +0200

    Created User class
    Created Class and added variables

---
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/User.java
@@ -0,0 +1,15 @@
+package se.chalmers.group22.gymcompanion.Model;
+
+import java.util.List;
+
+public class User {
+
+    List<User> friends;
+    String name;
+    String gym;
+    int age;
+    int weight;
+    boolean isBeginner;
+
+
+}
