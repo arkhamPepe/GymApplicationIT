da0dcb7f770ca52aa6402f9aeb587881eb036f64
ChalmersBock <erikboc@student.chalmers.se>
2018-09-24 09:08:25 +0200

    Implemented methods in Schedule
    Implemented addRoutine, removeRoutine and getRoutine in a way that passed their corresponding tests. Also created class Day to store a certain time.

--- GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/Schedule.java
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/Schedule.java
@@ -2,29 +2,28 @@ package se.chalmers.group22.gymcompanion.Model;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Getter
 public class Schedule {
 
     private List<Routine> routines = new ArrayList<>();
     private Reminder reminder;
+    private Map<Day, Routine> schedule = new HashMap<>();
 
     public Schedule(){
 
     }
 
-    public void addRoutine(Routine r, Date d){
-
+    public void addRoutine(Routine r, Day d){
+        schedule.put(d,r);
     }
 
-    public void removeRoutine(Routine r, Date d){
-
+    public void removeRoutine(Day d){
+        schedule.remove(d);
     }
 
-    public Routine getRoutine(Date d){
-        return null;
+    public Routine getRoutine(Day d){
+        return schedule.get(d);
     }
 }
