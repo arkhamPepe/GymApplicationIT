50ae13b60c2363ce428786b2b45fa5c71ccde08b
Marcus Svensson <makkan-@live.se>
2018-09-24 08:16:13 +0200

    Changed back gradle.settings

--- GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/Schedule.java
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/Schedule.java
@@ -1,9 +1,13 @@
 package se.chalmers.group22.gymcompanion.Model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Getter
 public class Schedule {
 
     private List<Routine> routines = new ArrayList<>();
