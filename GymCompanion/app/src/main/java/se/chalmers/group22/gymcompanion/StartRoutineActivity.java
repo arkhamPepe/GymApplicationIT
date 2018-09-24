fd8eef23de0c2d1ce372c18ee3113c6d40e06b67
Augustas :-( <augustas_age@hotmail.com>
2018-09-24 17:16:36 +0200

    Made an Activity for the "startRoutine"-page
    Enables the creation of the GUI

---
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/StartRoutineActivity.java
@@ -0,0 +1,13 @@
+package se.chalmers.group22.gymcompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartRoutineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_routine);
+    }
+}
