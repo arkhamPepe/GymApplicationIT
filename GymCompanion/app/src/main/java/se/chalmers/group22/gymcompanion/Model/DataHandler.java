bdc257f13fb68db0f7ad92bc82c48d5d2e2a8078
ChalmersBock <erikboc@student.chalmers.se>
2018-09-28 08:52:45 +0200

    Implemented StrategyPattern for sort and filter

--- GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/DataHandler.java
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/DataHandler.java
@@ -1,12 +1,9 @@
 package se.chalmers.group22.gymcompanion.Model;
 
-import se.chalmers.group22.gymcompanion.Enums.FILTER_MODE;
-import se.chalmers.group22.gymcompanion.Enums.MUSCLE_GROUP;
-import se.chalmers.group22.gymcompanion.Enums.SORT_MODE;
+import se.chalmers.group22.gymcompanion.Model.Strategies.FilterStrategy.FilterStrategy;
+import se.chalmers.group22.gymcompanion.Model.Strategies.SortingStrategy.SortingStrategy;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class DataHandler {
@@ -36,50 +33,12 @@ public class DataHandler {
         return null;
     }
 
-    public void sort(List<? extends ISortable> list, SORT_MODE mode){
-
-        switch(mode){
-            case ALPHABETIC_ASC:
-                list.sort(Comparator.comparing(ISortable::getName));
-                break;
-            case ALPHABETIC_DESC:
-                list.sort(Comparator.comparing(ISortable::getName));
-                Collections.reverse(list);
-                break;
-            case DIFFICULTY_ASC:
-                list.sort(Comparator.comparingDouble(ISortable::getDifficulty));
-            case DIFFICULTY_DESC:
-                list.sort(Comparator.comparingDouble(ISortable::getDifficulty));
-                Collections.reverse(list);
-                break;
-            default:
-                break;
-        }
+    public void sort(List<? extends ISortable> list, SortingStrategy strat){
+        strat.sort(list);
     }
 
-    public <T extends ISortable> List<T> filter(List<T> toBeFiltered, FILTER_MODE mode){
+    public <T extends ISortable> List<T> filter(List<T> toBeFiltered, FilterStrategy filter){
         List<T> newList = new ArrayList<>(toBeFiltered);
-
-        switch(mode){
-            case BEGINNER:
-                findBeginner(newList, toBeFiltered);
-                break;
-        }
-        return newList;
-    }
-
-
-    private <T extends ISortable> void findBeginner(List<T> newlist, List<T> oldlist ){
-        sort(oldlist, SORT_MODE.DIFFICULTY_ASC);
-
-        for(MUSCLE_GROUP mg : MUSCLE_GROUP.values()) {
-            for (T re: oldlist) {
-                if(re.containsMuscleGroup(mg) && !newlist.contains(re)){
-                    newlist.add(re);
-                    break;
-                }
-            }
-        }
-
+        return filter.filter(newList);
     }
 }
