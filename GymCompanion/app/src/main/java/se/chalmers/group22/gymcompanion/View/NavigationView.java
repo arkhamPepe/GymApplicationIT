8760b4c711468d53e64d1980e6e32751b63ea4c1
arkhamPepe <alexander.s.b@hotmail.com>
2018-09-28 13:40:44 +0200

    Added NavigationView

---
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/View/NavigationView.java
@@ -0,0 +1,14 @@
+package se.chalmers.group22.gymcompanion.View;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import se.chalmers.group22.gymcompanion.R;
+
+public class NavigationView extends LinearLayout {
+    public NavigationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        //init(context, attrs);
+    }
+}
