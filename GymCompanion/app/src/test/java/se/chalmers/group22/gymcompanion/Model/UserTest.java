16ae0e8c0850093ef288e63332ff3e26cd6da395
Augustas :-( <augustas_age@hotmail.com>
2018-10-01 15:44:22 +0200

    added information in Usertest

--- GymCompanion/app/src/test/java/se/chalmers/group22/gymcompanion/Model/UserTest.java
+++ GymCompanion/app/src/test/java/se/chalmers/group22/gymcompanion/Model/UserTest.java
@@ -1,8 +1,22 @@
 package se.chalmers.group22.gymcompanion.Model;
 
 import org.junit.Test;
+import se.chalmers.group22.gymcompanion.Model.Exercises.CardioExercise;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class UserTest {
+    private List<User> friends = new ArrayList<>();
+    private List<Routine> routines = new ArrayList<>();
+    private String userName = "Bock Ridenmakkok";
+    private String gym = "My House";
+    private int age = 15;
+    private int weight = 69;
+    private boolean isBeginner = false;
+
+    private User user = new User(friends,routines,userName,gym,age,weight,isBeginner);
 
     @Test
     public void startRoutineTest(){
