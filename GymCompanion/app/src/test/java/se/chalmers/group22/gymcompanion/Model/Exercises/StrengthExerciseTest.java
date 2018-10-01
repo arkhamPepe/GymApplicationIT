d1e932ae7cf64e107864c4143e1e9639addd89c3
danielolssonIT <31478577+danielolssonIT@users.noreply.github.com>
2018-10-01 21:26:07 +0200

    Fixed TimedExercise constructor parameters, added tests for StrengthExercise and CardioExercise

---
+++ GymCompanion/app/src/test/java/se/chalmers/group22/gymcompanion/Model/Exercises/StrengthExerciseTest.java
@@ -0,0 +1,85 @@
+package se.chalmers.group22.gymcompanion.Model.Exercises;
+
+import org.junit.Before;
+import org.junit.Test;
+import se.chalmers.group22.gymcompanion.Enums.INTENSITY;
+import se.chalmers.group22.gymcompanion.Enums.MUSCLE_GROUP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class StrengthExerciseTest {
+
+    private StrengthExercise strengthExercise;
+    private List<MUSCLE_GROUP> muscleGroups;
+    private List<Integer> reps;
+    private List<Double> kilograms;
+    private INTENSITY intensity;
+
+    @Before
+    public void init() {
+        this.muscleGroups = new ArrayList<>();
+        this.reps = new ArrayList<>();
+        this.kilograms = new ArrayList<>();
+        this.intensity = INTENSITY.MEDIUM;
+        this.strengthExercise = new StrengthExercise(
+                "Exercise 1",
+                2.3,
+                muscleGroups,
+                "A simple exercise",
+                null,
+                reps,
+                3,
+                kilograms,
+                intensity
+        );
+    }
+
+    @Test
+    public void containsMuscleGroupTest() {
+        assertFalse(strengthExercise.containsMuscleGroup(MUSCLE_GROUP.QUADS));
+
+        muscleGroups.add(MUSCLE_GROUP.BICEPS);
+        muscleGroups.add(MUSCLE_GROUP.QUADS);
+        muscleGroups.add(MUSCLE_GROUP.LOWER_BACK);
+
+        assertTrue(strengthExercise.containsMuscleGroup(MUSCLE_GROUP.QUADS));
+        assertFalse(strengthExercise.containsMuscleGroup(MUSCLE_GROUP.GLUTES));
+    }
+
+    @Test
+    public void isCompletedTest() {
+        assertFalse(strengthExercise.isCompleted());
+        strengthExercise.complete();
+        assertTrue(strengthExercise.isCompleted());
+    }
+
+    @Test
+    public void getKilogramsTest(){
+        assertEquals(0, strengthExercise.getKilograms().size());
+
+        kilograms.add(5.0);
+        kilograms.add(5.1);
+        kilograms.add(17.6);
+
+        assertEquals(3, strengthExercise.getKilograms().size());
+        assertEquals(17.6, strengthExercise.getKilograms().get(2), 0.01);
+    }
+
+    @Test
+    public void getIntensityTest(){
+        assertEquals(INTENSITY.MEDIUM, strengthExercise.getIntensity());
+    }
+
+    @Test
+    public void getSetsTest(){
+        assertEquals(3, strengthExercise.getSets());
+    }
+
+    @Test
+    public void getRepsTest(){
+        assertEquals(0, strengthExercise.getRepetitions().size());
+    }
+}
