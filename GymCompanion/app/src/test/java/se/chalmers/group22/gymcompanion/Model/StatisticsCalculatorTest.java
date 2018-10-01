82e8ebcbb9fced45d00a9b0aa617ec4cce71de20
Augustas :-( <augustas_age@hotmail.com>
2018-10-01 17:05:23 +0200

    Reworked StatisticsCalculatorTest

--- GymCompanion/app/src/test/java/se/chalmers/group22/gymcompanion/Model/StatisticsCalculatorTest.java
+++ GymCompanion/app/src/test/java/se/chalmers/group22/gymcompanion/Model/StatisticsCalculatorTest.java
@@ -1,28 +1,41 @@
 package se.chalmers.group22.gymcompanion.Model;
 
+import org.junit.Before;
 import org.junit.Test;
+import se.chalmers.group22.gymcompanion.Model.Exercises.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StatisticsCalculatorTest {
+    User user;
 
-    public StatisticsCalculatorTest(){
+    private String userName;
+    private String gym;
+    private int age;
+    private int weight;
+    private boolean isBeginner;
+
+    private List<Exercise> exercises;
+    private List<Routine> routines;
+
+
+    @Before
+    public void init(){
+        userName = "BOOOOOOOOOOOOOCK";
+        gym = "Welcome to my mine";
+        age = 12;
+        weight = 2;
+        isBeginner = false;
+
+        exercises = new ArrayList<>();
+        routines = new ArrayList<>();
+
+        user = new User(null,routines,userName,gym,age,weight,isBeginner);
     }
-
 
     @Test
     public void getSpecificExerciseTest(){
     }
-
-    @Test
-    public void calculateStrengthExerciseTest(){
-
-    }
-
-    @Test
-    public void calculateTimedExerciseTest(){
-
-    }
-
-
-
 
 }
