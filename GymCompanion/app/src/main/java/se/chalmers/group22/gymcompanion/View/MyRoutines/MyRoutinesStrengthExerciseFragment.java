9d091b4209f0ada3ef740937b09c75eee3eeb8f3
Augustas :-( <augustas_age@hotmail.com>
2018-10-05 13:54:00 +0200

    added an inflater

--- GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/View/MyRoutines/MyRoutinesStrengthExerciseFragment.java
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/View/MyRoutines/MyRoutinesStrengthExerciseFragment.java
@@ -20,5 +20,6 @@ public class MyRoutinesStrengthExerciseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_routine_setstrengthexercise, container, false);
     }
 }
