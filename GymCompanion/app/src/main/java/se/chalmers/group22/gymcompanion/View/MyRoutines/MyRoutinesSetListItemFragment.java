0efbeadd06b9b540ef7c4aae238a9f374e5258ce
Augustas :-( <augustas_age@hotmail.com>
2018-10-08 10:44:15 +0200

    Created new listitem for a specific exercise's sets. Created a fragment class for it.
    The listitem is a display of the sets of a specific exercises with information on repetitions and weight.

---
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/View/MyRoutines/MyRoutinesSetListItemFragment.java
@@ -0,0 +1,25 @@
+package se.chalmers.group22.gymcompanion.View.MyRoutines;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import se.chalmers.group22.gymcompanion.R;
+
+public class MyRoutinesSetListItemFragment extends Fragment {
+    public static MyRoutinesSetListItemFragment newInstance() {
+        MyRoutinesSetListItemFragment fragment = new MyRoutinesSetListItemFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.listitem_my_routines_sets, container, false);
+    }
+}
