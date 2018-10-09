5af0e88dd0d6e89d794f778aa438913b369275a2
Augustas :-( <augustas_age@hotmail.com>
2018-10-09 09:51:17 +0200

    Created an listitem for exercises in layout. Also created an adapter-class for the exercise list.

---
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/View/MyRoutines/ExerciseListAdapter.java
@@ -0,0 +1,19 @@
+package se.chalmers.group22.gymcompanion.View.MyRoutines;
+
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import se.chalmers.group22.gymcompanion.Model.Exercises.Exercise;
+import se.chalmers.group22.gymcompanion.R;
+
+public class ExerciseListAdapter extends ArrayAdapter {
+
+    //to reference the activity
+    private Activity context;
+
+    //to store exercises
+    private Exercise[] exercises;
+
+    public ExerciseListAdapter(Activity context, Exercise[] exercises){
+        super(context, R.layout.listitem);
+    }
+}
