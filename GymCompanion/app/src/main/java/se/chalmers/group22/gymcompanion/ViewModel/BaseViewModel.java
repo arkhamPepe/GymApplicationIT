eefac8372f727b59bb9ee0c625df3b38764cb102
danielolssonIT <31478577+danielolssonIT@users.noreply.github.com>
2018-10-12 11:05:59 +0200

    Fixed setter method for BaseViewModel

--- GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/ViewModel/BaseViewModel.java
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/ViewModel/BaseViewModel.java
@@ -5,11 +5,15 @@ import se.chalmers.group22.gymcompanion.Model.GymCompanion;
 public abstract class BaseViewModel {
     private static GymCompanion model;
 
-    public BaseViewModel(GymCompanion model){
-        BaseViewModel.model = model;
+    public BaseViewModel(){
+
     }
 
     public static GymCompanion getModel() {
         return model;
+    }
+
+    public static void setModel(GymCompanion model) {
+        BaseViewModel.model = model;
     }
 }
