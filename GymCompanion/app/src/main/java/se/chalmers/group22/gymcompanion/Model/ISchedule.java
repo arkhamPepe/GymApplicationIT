9e167ccf104731af136bac5378ebcd2f78c2cc9e
arkhamPepe <alexander.s.b@hotmail.com>
2018-10-13 21:46:58 +0200

    Added addRoutine and removeRoutine to ISchedule

--- GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/ISchedule.java
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/Model/ISchedule.java
@@ -6,4 +6,8 @@ public interface ISchedule {
     String getRoutineNameFromDay(Calendar day);
 
     boolean dayHasRoutine(Calendar day);
+
+    void addRoutine(Routine r, Calendar day);
+
+    void removeRoutine(Calendar day);
 }
