f6d5c0f144903ccf42c2ed3212e618566f544f06
danielolssonIT <31478577+danielolssonIT@users.noreply.github.com>
2018-10-18 13:37:10 +0200

    Added test for Parser

--- GymCompanion/app/src/androidTest/java/se/chalmers/group22/gymcompanion/Model/ParserTest.java
+++ GymCompanion/app/src/androidTest/java/se/chalmers/group22/gymcompanion/Model/ParserTest.java
@@ -25,6 +25,7 @@ public class ParserTest {
         Exercise e1 = exercises.get(0);
         Log.d("ParserTest", e1.getDescription());
         assertEquals("Bench Press", e1.getName());
+        assertEquals("A very stronk exercise", e1.getDescription());
     }
 
     @Test
@@ -32,5 +33,6 @@ public class ParserTest {
         List<Routine> parseRoutines = parser.getRoutines();
         Routine r1 = parseRoutines.get(0);
         assertEquals("Big Arms Workout", r1.getName());
+        assertEquals("Bench Press", r1.getExercises().get(0).getName());
     }
 }
