99ec1143572e4d6a1eabb14b80334a891e94bb08
danielolssonIT <31478577+danielolssonIT@users.noreply.github.com>
2018-10-19 15:54:28 +0200

    Update Android Test Suite

--- GymCompanion/app/src/androidTest/java/se/chalmers/group22/gymcompanion/Model/ParserTest.java
+++ GymCompanion/app/src/androidTest/java/se/chalmers/group22/gymcompanion/Model/ParserTest.java
@@ -1,19 +1,15 @@
 package se.chalmers.group22.gymcompanion.Model;
 
-import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import se.chalmers.group22.gymcompanion.Model.Exercises.Exercise;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-@RunWith(AndroidJUnit4.class)
 public class ParserTest {
-
     private Parser parser;
 
     @Before
