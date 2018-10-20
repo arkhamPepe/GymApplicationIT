26b75cf71f14c38417c850e25d8b5911457e1631
arkhamPepe <alexander.s.b@hotmail.com>
2018-10-20 14:19:08 +0200

    Removed drawGraphTest from StatisticsStartFragment

--- GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/View/Statistics/StatisticsStartFragment.java
+++ GymCompanion/app/src/main/java/se/chalmers/group22/gymcompanion/View/Statistics/StatisticsStartFragment.java
@@ -73,41 +73,9 @@ public class StatisticsStartFragment extends Fragment implements Observer {
         drawGraph();
     }
 
-    private void drawGraphTest(){
-        // generate Dates
-        Calendar calendar = Calendar.getInstance();
-        Date d1 = calendar.getTime();
-        calendar.add(Calendar.DATE, 1);
-        Date d2 = calendar.getTime();
-        calendar.add(Calendar.DATE, 1);
-        Date d3 = calendar.getTime();
-
-        GraphView graph = (GraphView) getView().findViewById(R.id.start_graph);
-
-// you can directly pass Date objects to DataPoint-Constructor
-// this will convert the Date to double via Date#getTime()
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(d1, 1),
-                new DataPoint(d2, 5),
-                new DataPoint(d3, 3)
-        });
-
-        graph.addSeries(series);
-
-// set date label formatter
-        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
-        graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
-
-// set manual x bounds to have nice steps
-        graph.getViewport().setMinX(d1.getTime());
-        graph.getViewport().setMaxX(d3.getTime());
-        graph.getViewport().setXAxisBoundsManual(true);
-
-// as we use dates as labels, the human rounding to nice readable numbers
-// is not necessary
-        graph.getGridLabelRenderer().setHumanRounding(false);
-    }
-
+    /** drawGraph
+     * Purpose: Draw the graph from scratch
+     */
     private void drawGraph(){
         GraphView graph = getView().findViewById(R.id.start_graph);
 
